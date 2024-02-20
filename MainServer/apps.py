449593d0c6269dbd5e024813d95f77ed8f93fb4d
ykkssyaa <mr.ender.03@mail.ru>
2024-02-20 16:56:46 +0300

    little fixes and settings

--- MainServer/apps.py
+++ MainServer/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CollgameConfig(AppConfig):
+class MainServerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'CollGame'
+    name = 'MainServer'
