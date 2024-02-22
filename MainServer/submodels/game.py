8b7859a574e8a7b2445e5a675a6ed9b074a563ae
ykkssyaa <mr.ender.03@mail.ru>
2024-02-22 18:41:37 +0300

    add submodels dir

---
+++ MainServer/submodels/game.py
@@ -0,0 +1,46 @@
+from django.db import models
+from taggit.managers import TaggableManager
+
+
+class Game(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    time_created = models.DateTimeField(auto_now_add=True)
+    time_updated = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+
+    slug = models.SlugField(max_length=255, db_index=True, unique=True)
+    studio = models.ForeignKey('GameStudio', on_delete=models.PROTECT, null=True)
+
+    tags = TaggableManager()
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/games/{self.slug}/'
+
+
+class GameStudio(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Platform(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Release(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    platform = models.ForeignKey(Platform, on_delete=models.CASCADE)
+    release_date = models.DateField()
+
+
+class GamesLinks(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    link = models.URLField()
