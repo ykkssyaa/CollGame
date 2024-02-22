78c002fc4e13f6c2d06fea2b841cc034cfd3a9c4
ykkssyaa <mr.ender.03@mail.ru>
2024-02-22 18:41:59 +0300

    register models in admin panel

--- MainServer/admin.py
+++ MainServer/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Game, GameStudio, GamesLinks, Platform, Release
+
+admin.site.register(Game)
+admin.site.register(GameStudio)
+admin.site.register(GamesLinks)
+admin.site.register(Platform)
+admin.site.register(Release)
+
+
